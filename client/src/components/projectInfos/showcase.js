e27c3694e414ee525e7295dadac0d7352e92f14b
Kakapo <mastertroll123fig@gmail.com>
2024-10-29 11:53:39 +1300

    Mobile Version

--- client/src/components/projectInfos/showcase.js
+++ client/src/components/projectInfos/showcase.js
@@ -1,28 +1,46 @@
 import React, { Component } from "react";
 
 class Showcase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expandedIndex: null, // Track which item is expanded
+    };
+  }
+
+  toggleExpand = (index) => {
+    this.setState((prevState) => ({
+      expandedIndex: prevState.expandedIndex === index ? null : index,
+    }));
+  };
+
   render() {
     const { devs } = this.props;
+    const { expandedIndex } = this.state;
 
     return (
       <div>
         <div className="showcaseContainer">
           {devs.map((dev, index) => (
-            <div key={index} className="showcaseItem">
+            <div
+              key={index}
+              className={`showcaseItem ${expandedIndex === index ? "expanded" : ""}`}
+              onClick={() => this.toggleExpand(index)}
+            >
               <img src={dev.imageUrl} alt={dev.name} className="showcaseImg" />
               <div className="showcaseDetails">
                 <h2>{dev.name}</h2>
                 <p>{dev.description}</p>
                 <p>
                   <strong>Skills:</strong>
-                  <ul>
-                    {/* Spliting the skills string by ', ' and mapping over it */}
+                  <span className="skills">
                     {dev.skills.split(", ").map((skill, skillIndex) => (
-                      <li key={skillIndex}>{skill}</li>
+                      <span key={skillIndex} className="skill">
+                        {skill}{skillIndex < dev.skills.split(", ").length - 1 && ", "}
+                      </span>
                     ))}
-                  </ul>
+                  </span>
                 </p>
-                {/* Conditionally renders a link separately if the URL exists */}
                 {dev.url && (
                   <a href={dev.url} target="_blank" rel="noopener noreferrer" className="projLink">
                     Visit Project
