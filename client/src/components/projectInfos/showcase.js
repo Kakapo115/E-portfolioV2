f7bc23d5b29bdd4f45293cf1eaf66a800ccd086e
Kakapo <mastertroll123fig@gmail.com>
2025-06-03 15:41:54 +1200

    Revamping

--- client/src/components/projectInfos/showcase.js
+++ client/src/components/projectInfos/showcase.js
@@ -4,51 +4,87 @@ class Showcase extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      expandedIndex: null, // Track expanded item
+      currentIndex: 0,
+      isHovered: false,
     };
+    this.slideInterval = null;
   }
 
-  toggleExpand = (index) => {
+  componentDidMount() {
+    this.startAutoSlide();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.slideInterval);
+  }
+
+  startAutoSlide = () => {
+    this.slideInterval = setInterval(() => {
+      if (!this.state.isHovered) {
+        this.goToNextSlide();
+      }
+    }, 4000); // Slide every 4 seconds
+  };
+
+  goToNextSlide = () => {
+    const { devs } = this.props;
     this.setState((prevState) => ({
-      expandedIndex: prevState.expandedIndex === index ? null : index,
+      currentIndex: (prevState.currentIndex + 1) % devs.length,
     }));
+  };
+
+  goToPreviousSlide = () => {
+    const { devs } = this.props;
+    this.setState((prevState) => ({
+      currentIndex:
+        (prevState.currentIndex - 1 + devs.length) % devs.length,
+    }));
+  };
+
+  handleMouseEnter = () => {
+    this.setState({ isHovered: true });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ isHovered: false });
   };
 
   render() {
     const { devs } = this.props;
-    const { expandedIndex } = this.state;
+    const { currentIndex } = this.state;
+    const dev = devs[currentIndex];
 
     return (
-      <div>
-        <div className="showcaseContainer">
-          {devs.map((dev, index) => (
-            <div
-              key={index}
-              className={`showcaseItem ${expandedIndex === index ? "expanded" : ""}`}
-              onClick={() => this.toggleExpand(index)}
-            >
-              <img src={dev.imageUrl} alt={dev.name} className="showcaseImg" />
-              <div className="showcaseDetails">
-                <h2>{dev.name}</h2>
-                <p>{dev.description}</p>
-                <p>
-                  <strong>Skills:</strong>
-                  <span className="skills">
-                    {dev.skills.split(", ").map((skill, skillIndex) => (
-                      <span key={skillIndex} className="skill">
-                        {skill}{skillIndex < dev.skills.split(", ").length - 1 && ", "}
-                      </span>
-                    ))}
-                  </span>
-                </p>
-                {dev.url && (
-                  <a href={dev.url} target="_blank" rel="noopener noreferrer" className="projLink">
-                    Visit Project
-                  </a>
-                )}
-              </div>
-            </div>
-          ))}
+      <div
+        className="slideshowContainer"
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        <div className="slide fadeIn">
+          <img src={dev.imageUrl} alt={dev.name} className="slideImage" />
+          <div className="slideDetails">
+            <h2>{dev.name}</h2>
+            <p>{dev.description}</p>
+            <p>
+              <strong>Skills:</strong>{" "}
+              {dev.skills.split(", ").map((skill, i) => (
+                <span key={i} className="skill">
+                  {skill}
+                  {i < dev.skills.split(", ").length - 1 && ", "}
+                </span>
+              ))}
+            </p>
+            {dev.url && (
+              <a
+                href={dev.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="projLink"
+              >
+                Visit Project
+              </a>
+            )}
+          </div>
         </div>
       </div>
     );
