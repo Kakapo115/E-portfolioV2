0f045fd490832cf7e6167853fc544abd9dee5e18
Kakapo <mastertroll123fig@gmail.com>
2025-06-03 15:48:42 +1200

    Revamping

--- client/src/components/project/projects.js
+++ client/src/components/project/projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Showcase from "../projectInfos/showcase";
+import Showcase from "/showcase";
 
 class projects extends Component {
   constructor(props) {
