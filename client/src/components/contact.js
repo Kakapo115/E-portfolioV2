56baf30ce3e57580b33dbb3364dd942d2937bc58
Kakapo <mastertroll123fig@gmail.com>
2025-06-09 13:20:20 +1200

    recaptcha

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import ReCAPTCHA from "react-google-recaptcha";
 
 class contact extends Component {
   constructor() {
@@ -8,37 +9,48 @@ class contact extends Component {
       name: "",
       email: "",
       message: "",
-      sending: false, // To track whether the email is currently being sent
+      captchaValue: null,
+      sending: false,
       sent: false,
     };
   }
 
   handleFormSubmit = async (e) => {
     e.preventDefault();
-    const { name, email, message } = this.state;
+    const { name, email, message, captchaValue } = this.state;
 
-    this.setState({ sending: true }); // Set sending state to true
+    if (!captchaValue) {
+      alert("Please complete the CAPTCHA.");
+      return;
+    }
+
+    this.setState({ sending: true });
 
     try {
       const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
-      // Send the form data to the backend API
       const response = await axios.post(`${apiBaseUrl}/api/dev/send-mail`, {
         name,
         email,
         message,
+        captcha: captchaValue, // optional if backend verifies
       });
       if (response.data.success) {
         this.setState({ sent: true, sending: false });
       }
     } catch (error) {
       console.error("Error sending email: ", error);
-      this.setState({ sending: false }); // Reset sending state in case of error
+      this.setState({ sending: false });
     }
   };
 
   handleInputChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
+
+  handleCaptchaChange = (value) => {
+    this.setState({ captchaValue: value });
+  };
+
   render() {
     const { name, email, message, sent, sending } = this.state;
 
@@ -74,9 +86,18 @@ class contact extends Component {
                   onChange={this.handleInputChange}
                 ></textarea>
               </div>
+
+              <div className="form-group">
+                <ReCAPTCHA
+                  sitekey="6LchC1orAAAAACMkDD_7KWCUBUyq9iV8VRn6VDxn"
+                  onChange={this.handleCaptchaChange}
+                />
+              </div>
+
               <div className="form-group">
                 <button type="submit">Send</button>
               </div>
+
               {sending && <p className="sending-message">Sending...</p>}
               {sent && <p className="sent-message">Email sent successfully!</p>}
             </form>
@@ -97,4 +118,5 @@ class contact extends Component {
     );
   }
 }
+
 export default contact;
