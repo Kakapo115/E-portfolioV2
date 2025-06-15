7b8af09716d0751897559b2738cc8e406a313017
Kakapo <mastertroll123fig@gmail.com>
2025-06-16 11:06:51 +1200

    fix?

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "../styles/SiteAnimations.css";
 
 class AboutSlideshow extends Component {
   constructor(props) {
--- client/src/components/project/showcase.js
+++ client/src/components/project/showcase.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "../styles/SiteAnimations.css";
 
 class Showcase extends Component {
   constructor(props) {
