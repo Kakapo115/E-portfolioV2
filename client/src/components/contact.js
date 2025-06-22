48508affe3b0401ed911781a6b8822df50bde3b7
Kakapo <mastertroll123fig@gmail.com>
2025-06-23 09:49:20 +1200

    4

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -69,69 +69,65 @@ class contact extends Component {
 
     return (
       <div className="pageContent">
-        <div className="contact">
-          <div className="contact-container">
-            <div className="contactform">
-              <h1>Let's Get In Touch</h1>
-              <p>Fill in the form below or download my CV.</p>
-              <form onSubmit={this.handleFormSubmit}>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    name="name"
-                    value={name}
-                    placeholder="Your Name"
-                    onChange={this.handleInputChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="email"
-                    name="email"
-                    value={email}
-                    placeholder="Your Email"
-                    onChange={this.handleInputChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    value={message}
-                    placeholder="Your Message"
-                    onChange={this.handleInputChange}
-                  ></textarea>
-                </div>
+        <div className="contact-container">
+          <div className="contactform">
+            <h1>Let's Get In Touch</h1>
+            <p>Fill in the form below or download my CV.</p>
+            <form onSubmit={this.handleFormSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="name"
+                  value={name}
+                  placeholder="Your Name"
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  placeholder="Your Email"
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <textarea
+                  name="message"
+                  value={message}
+                  placeholder="Your Message"
+                  onChange={this.handleInputChange}
+                ></textarea>
+              </div>
 
-                <div className="form-group recaptcha">
-                  <ReCAPTCHA
-                    ref={this.recaptchaRef}
-                    sitekey="6LchC1orAAAAACMkDD_7KWCUBUyq9iV8VRn6VDxn"
-                    onChange={this.handleCaptchaChange}
-                  />
-                </div>
+              <div className="form-group recaptcha">
+                <ReCAPTCHA
+                  ref={this.recaptchaRef}
+                  sitekey="6LchC1orAAAAACMkDD_7KWCUBUyq9iV8VRn6VDxn"
+                  onChange={this.handleCaptchaChange}
+                />
+              </div>
 
-                <div className="form-group">
-                  <button type="submit">Send</button>
-                </div>
+              <div className="form-group">
+                <button type="submit">Send</button>
+              </div>
 
-                {sending && <p className="sending-message">Sending...</p>}
-                {sent && (
-                  <p className="sent-message">Email sent successfully!</p>
-                )}
-              </form>
-            </div>
+              {sending && <p className="sending-message">Sending...</p>}
+              {sent && <p className="sent-message">Email sent successfully!</p>}
+            </form>
+          </div>
 
-            <div className="cv-container">
-              <h1>Download My CV</h1>
-              <p>Get a copy of my latest CV in PDF format:</p>
-              <a
-                href="assets/RickySyme-CV-SoftwareDev.docx.pdf"
-                download
-                className="download-btn"
-              >
-                Download CV (PDF)
-              </a>
-            </div>
+          <div className="cv-container">
+            <h1>Download My CV</h1>
+            <p>Get a copy of my latest CV in PDF format:</p>
+            <a
+              href="assets/RickySyme-CV-SoftwareDev.docx.pdf"
+              download
+              className="download-btn"
+            >
+              Download CV (PDF)
+            </a>
           </div>
         </div>
       </div>
